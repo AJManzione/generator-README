e5a417b7689ff86f751fe7b69f4ab1b079e3c778
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-18 16:57:36 -0600

    first question of readme-generator functional

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-console.log(process.argv);
+const inquirer = require('inquirer');
+const fs = require('fs')
+const { message } = require('statuses');
+const { title } = require('process');
+
+inquirer.prompt (
+    [
+        {
+            type: 'input'.
+            message = "What's the project title?",
+            name: 'title',
+            validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}}
+        }
+    ]
+)
+
+
+
+
+
+//console.log(process.argv);
+// I need to use argv to get the user input --
 
 
 //WHEN I am prompted for information about my application repository
 //THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
+
+
+
 //WHEN I enter my project title
 //THEN this is displayed as the title of the README
+
+
+
 
 //WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 //THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 
+
+
 //WHEN I choose a license for my application from a list of options
 //THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+
 
 //WHEN I enter my GitHub username
 //THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
 
+
+
 //WHEN I enter my email address
 //THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+
 
 //WHEN I click on the links in the Table of Contents
 //THEN I am taken to the corresponding section of the README
